5b1678d448e35ee796221f22669269e4298f7802
RobertoRyanRibeiro <roberto.ribeiro@toodoo.com.br>
2022-04-14 10:11:42 -0300

    Inicio Calculadora

---
+++ C#/Calculadora/Calculadora/Calculadora/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            
+            Console.WriteLine("Primeiro Valor");
+            float v1 = float.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Segundo Valor");
+            float v2 = float.Parse(Console.ReadLine());
+
+
+            float soma = v1+v2;
+
+            Console.WriteLine($"A soma de {v1} + {v2} = {soma}");
+        }
+    }
+}
