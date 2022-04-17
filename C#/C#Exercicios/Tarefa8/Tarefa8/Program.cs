9fa288cbf06eb0b11bc48c24e11343f9ec62f63e
RobertoRyanRibeiro <roberto.ribeiro@toodoo.com.br>
2022-04-17 14:24:59 -0300

    C#Exerc_Tarefa8

--- C#/C#Exercicios/Tarefa8/Tarefa8/Program.cs
+++ C#/C#Exercicios/Tarefa8/Tarefa8/Program.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Threading;
 using System.Globalization;
+using System.Text.RegularExpressions;
 
 namespace Tarefa8
 {
     internal class Program
     {
+        static Regex pattern = new Regex(@"\d{4}\-*\d{3,4}");
+
         static void Main(string[] args)
         {
             Menu();
@@ -13,59 +16,74 @@ namespace Tarefa8
 
         static void Menu()
         {
+
             Console.Clear();
             Console.WriteLine("Digite um numero de telefone : XXXX-XXXX");
             Console.WriteLine("========================================");
-            var telTxt = Console.ReadLine();
+            var tel = Console.ReadLine();
 
             try
             {
-                telTxt = AnalisarNum(telTxt);
-                ImprimirNum(telTxt);
+                Match matchTelFormat = pattern.Match(tel);
+                if (matchTelFormat.Success)
+                {
+                    AnalisarNum(tel);
+                }
+                else
+                {
+                    Console.Clear();
+                    ErrorMsg();
+                }
             }
-            catch (Exception ex)
+            catch (OverflowException ex)
             {
                 Console.WriteLine(ex.InnerException);
                 Console.WriteLine(ex.Message);
-                Thread.Sleep(1000);
-                Menu();
+                ErrorMsg();
             }
 
         }
 
-        static string AnalisarNum(string telTxt)
+        static void AnalisarNum(string tel)
         {
+            Console.Clear();
+            var aux = tel;
 
-            if (telTxt.Length > 8)
-                throw new Exception("O Numero ultrapassou o limite de digitos");
-
-            if (String.IsNullOrEmpty(telTxt) || String.IsNullOrWhiteSpace(telTxt))
-                throw new Exception("O Numero estar vazio");
-
-            var telX = telTxt.Split("-");
-            var tel = "";
-            //Verificando se e numero
-            for (var c=0; c < telTxt.Length; c++)
+            //Verificando se tem -
+            if (aux.Contains("-"))
             {
-                var numero = int.Parse(telX[c]);
-                tel = telX[c];
+                aux = Regex.Replace(tel, @"\-", "");
+            }
+            //Verificando se o numero tem menos de 7 digitos
+            if (aux.Length == 7)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Telefone possui 7 dígitos. Vou acrescentar o digito três na frente.");
+                aux += "3";
+                tel += "3";
+                Console.ForegroundColor = ConsoleColor.White;
             }
 
+            //Formatação
+            var telFixF = int.Parse(aux).ToString(@"0000-0000");
+            var telFix = int.Parse(aux).ToString(@"00000000");
+            ImprimirNum(tel, telFix, telFixF);
+        }
+
+        static void ImprimirNum(string tel, string telFix, string telFixF)
+        {
+            Console.WriteLine("Valida e corrige número de telefone");
             Console.WriteLine($"Telefone: {tel}");
-
-            if (tel.Length == 7)
-            {
-                Console.WriteLine("Telefone possui 7 digitos. Vou acrescentar o digito três na frente.");
-                tel.Insert(6, "3");
-            }
-
-            return tel;
+            Console.WriteLine("===================================");
+            Console.WriteLine($"Telefone Corrigido com formatação {telFixF}");
+            Console.WriteLine($"Telefone Corrigido sem formatação {telFix}");
         }
 
-        static void ImprimirNum(string txt)
+        static void ErrorMsg()
         {
-            Console.WriteLine($"Telefone Corrigido sem formatação {txt}");
-            Console.WriteLine($"Telefone Corrigido sem formatação {}");
+            Console.WriteLine("Error: Formatação de Numero errado");
+            Thread.Sleep(1000);
+            Menu();
         }
     }
 }
