9489adc8fbce31c0f58bd7136c56bd31849fe4ac
RobertoRyanRibeiro <roberto.ribeiro@toodoo.com.br>
2022-06-28 06:37:48 -0300

    AtividadesEF

---
+++ Aulas/EF/Atividades/Atividade5/Atividade5/Data/Atividade5DbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade5.Data
+{
+    public class Atividade5DbContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+            => builder.UseSqlServer("Server = localhost,1433;Database = faculdade,User ID=sa,Password = 1q2w3e4r@#$");
+            
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
